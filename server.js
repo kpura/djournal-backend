62b8362685ec3e8e5d26563dd8bfcf0286024aee
unknown <kayepura@thelewiscollege.edu.ph>
2025-04-03 07:00:32 +0800

    latest

--- server.js
+++ server.js
@@ -29,10 +29,11 @@ const pool = mysql.createPool({
 
 // Middleware
 app.use(cors());
-app.use(express.json());
-app.use(bodyParser.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
